ec939aec27799036c335c5adb4195a7b09233bb4
Roey Darwish Dror <roey.ghost@gmail.com>
2018-08-04 21:05:15 +0300

    Add resize ability

--- daily_photo.py
+++ daily_photo.py
@@ -1,18 +1,32 @@
 import random
 import telegram
 import os
+import io
+from PIL import Image
+
+_PHOTO_SIZE_LIMIT = 10 * 1024 ** 2
+
+def _get_resized_photo(path):
+    image = Image.open(path)
+    image.thumbnail((1280, 720))
+
+    fp = io.BytesIO()
+    image.save(fp, format="jpeg")
+    fp.seek(0)
+    return fp
+
 
 def _get_photo_path():
     directory = os.environ["DAILY_PHOTO_DIRECTORY"]
     return os.path.join(directory, random.choice(os.listdir(directory)))
+
 
 def main():
     photo_path = _get_photo_path()
     chat_id = os.environ["DAILY_PHOTO_CHAT_ID"]
     bot = telegram.Bot(token=os.environ["DAILY_PHOTO_BOT_TOKEN"])
 
-    with open(photo_path, "rb") as f:
-        bot.send_photo(chat_id=chat_id, photo=f)
+    bot.send_photo(chat_id=chat_id, photo=_get_resized_photo(photo_path))
 
     os.unlink(photo_path)
 
