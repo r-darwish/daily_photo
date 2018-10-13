c6097d1e2addfebfebad03ce43c706918e6d49ef
Roey Darwish Dror <roey.ghost@gmail.com>
2018-10-13 22:53:25 +0300

    Support Sentry

--- daily_photo.py
+++ daily_photo.py
@@ -1,7 +1,8 @@
-import random
-import telegram
-import os
 import io
+import os
+import random
+import sentry_sdk
+import telegram
 from PIL import Image
 
 _PHOTO_SIZE_LIMIT = 10 * 1024 ** 2
@@ -22,6 +23,7 @@ def _get_photo_path():
 
 
 def main():
+    sentry_sdk.init(os.environ.get("DAILY_PHOTO_SENTRY_DSN"))
     photo_path = _get_photo_path()
     chat_id = os.environ["DAILY_PHOTO_CHAT_ID"]
     bot = telegram.Bot(token=os.environ["DAILY_PHOTO_BOT_TOKEN"])
